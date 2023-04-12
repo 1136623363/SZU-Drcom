f26ba16afa798015ff39c04c9b0ef19ea5d4275c
1136623363 <72068332+1136623363@users.noreply.github.com>
2023-04-12 19:12:32 +0800

    Add files via upload

---
+++ natfrpc.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# 下载并重命名frpc客户端
+wget https://getfrp.sh/d/frpc_linux_armv7
+mv frpc_linux_armv7 /sbin/natfrpc
+chmod a+wx /sbin/natfrpc
+
+# 创建并配置启动脚本
+cat << 'EOF' > /etc/init.d/natfrpc
+#!/bin/sh /etc/rc.common
+
+USE_PROCD=1
+START=90
+
+start_service() {
+    procd_open_instance natfrpc
+    procd_set_param command /sbin/natfrpc
+
+    procd_append_param command -f p63xqglrhn7hc9iocgldacbb9mkincg7:3152 --update
+ 
+    procd_set_param env LANG=zh_CN.UTF-8
+    procd_set_param limits nofile="unlimited"
+    procd_set_param respawn 300 5 10
+    procd_set_param stdout 1
+    procd_set_param stderr 1
+    procd_add_jail natfrp log
+    procd_close_instance
+}
+EOF
+
+# 去除 Windows 换行符并赋予可执行权限
+sed -i 's/\r//' /etc/init.d/natfrpc
+chmod +x /etc/init.d/natfrpc
+
+# 启动服务并设置开机自启动
+/etc/init.d/natfrpc start
+/etc/init.d/natfrpc enable
